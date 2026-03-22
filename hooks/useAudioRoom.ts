33bbd711930dac069393618ac12cb81bbe09aa5a
dev-nathaniel <nathanielolowofoyeku@gmail.com>
2026-03-22 11:04:17 +0000

    change server_url

--- hooks/useAudioRoom.ts
+++ hooks/useAudioRoom.ts
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { Device, types } from 'mediasoup-client';
 
-const SERVER_URL = 'http://138.68.181.154:4000';
+const SERVER_URL = 'https://liveserver.usedistress.com';
 
 export function useAudioRoom(roomId: string, userId: string, role: 'speaker' | 'listener') {
   const [isConnected, setIsConnected] = useState(false);
